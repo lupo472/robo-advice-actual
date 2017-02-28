946fbd21e29161f4430ffe3b68e6b7a3cb79894f
Cristian Laurini <cristian.laurini@gmail.com>
2017-02-28 18:26:16 +0100

    spring configuration

--- src/main/java/it/uiip/digitalgarage/roboadvice/RoboadviceApplication.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/RoboadviceApplication.java
@@ -1,9 +1,11 @@
-package it.uiip.digitalgarage.roboadvice.controller;
+package it.uiip.digitalgarage.roboadvice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class RoboadviceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/config/WebConfig.java
@@ -0,0 +1,16 @@
+package it.uiip.digitalgarage.roboadvice.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/roboadvice/").setViewName("forward:/index.html");
+	}
+
+}
