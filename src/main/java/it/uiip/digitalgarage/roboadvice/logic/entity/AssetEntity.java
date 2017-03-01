6bc93214ca5bb052e63d81650bda1d54de21d368
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-01 19:32:00 +0100

    Created Asset Entity, Repository and Controller

--- src/main/java/it/uiip/digitalgarage/roboadvice/logic/entity/AssetEntity.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/logic/entity/AssetEntity.java
@@ -2,28 +2,40 @@ package it.uiip.digitalgarage.roboadvice.logic.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 
-//@Entity
-//@Table(name="asset")
+@Entity
+@Table(name="asset")
 public @Data class AssetEntity {
 	
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-//	private Long id;
-//	
-//	
-//	private AssetClassEntity assetClass;
-//
-//	private String name;
-//	private String dataSource;
-//	private BigDecimal percentage;
-//	private int remarksIndex;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne()
+	@JoinColumn(name = "id_asset_class")
+	private AssetClassEntity assetClass;
+
+	@Column(name = "name", nullable = false, unique = true)
+	private String name;
+	
+	@Column(name = "data_source", nullable = false, unique = true)
+	private String dataSource;
+	
+	@Column(name = "percentage", nullable = false)
+	private BigDecimal percentage;
+	
+	@Column(name = "remarks_index", nullable = false)
+	private int remarksIndex;
 	
 }
