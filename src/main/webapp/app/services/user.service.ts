ad20ccfe9df270c106a0f73b5a5ec45b481b2fde
alberto.giovanelli <alberto.giovanelli@icloud.com>
2017-03-01 15:18:30 +0100

    Modified dashboard

--- src/main/webapp/app/services/user.service.ts
+++ src/main/webapp/app/services/user.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import { GeneralService } from './general.service';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserService {
-  
+
   public url:GeneralService;
-  
+
   constructor(private http:Http, public GeneralUrl: GeneralService) {
     this.url = GeneralUrl;
   }
@@ -16,7 +15,7 @@ export class UserService {
     return this.http.post(this.url + 'login',{"email": "peter@klaven","password": "cityslicka"})
       .map(response => response.json());
   }
-  
+
   register(){
     return this.http.post(this.url + 'register',{"email":"sydney@fife","password":"pistol"})
       .map(response => response.json());
