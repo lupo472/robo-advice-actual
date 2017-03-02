c6e066138522c0da76d9db2bb45e863ec1c0c532
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-02 18:00:06 +0100

    Created Controller and Repository for Capital

--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/entity/CapitalEntity.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/entity/CapitalEntity.java
@@ -4,14 +4,18 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "capital")
 public @Data class CapitalEntity {
 
 	@Id
---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/service/controller/CapitalController.java
@@ -0,0 +1,10 @@
+package it.uiip.digitalgarage.roboadvice.service.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class CapitalController extends GenericController {
+
+}
