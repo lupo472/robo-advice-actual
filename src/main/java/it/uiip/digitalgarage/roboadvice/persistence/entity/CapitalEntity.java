623b4bb311251438854b75ed165b885d77f10a6a
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-02 17:55:35 +0100

    created CapitalEntity

---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/entity/CapitalEntity.java
@@ -0,0 +1,31 @@
+package it.uiip.digitalgarage.roboadvice.persistence.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+public @Data class CapitalEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+	
+	@ManyToOne()
+	@JoinColumn(name = "id_user", nullable = false)
+    private Long idUser;
+	
+	@Column(name = "amount", nullable = false, precision = 14, scale = 4)
+	private BigDecimal amount;
+	
+	@Column(name = "date")
+    private LocalDate date;
+
+}
--- src/main/java/it/uiip/digitalgarage/roboadvice/service/controller/PortfolioController.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/service/controller/PortfolioController.java
@@ -3,10 +3,6 @@ package it.uiip.digitalgarage.roboadvice.service.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by Luca on 02/03/2017.
- */
-
 @CrossOrigin("*")
 @RestController
 public class PortfolioController {
