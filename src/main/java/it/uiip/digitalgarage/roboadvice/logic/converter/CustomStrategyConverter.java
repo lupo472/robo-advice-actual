4e75b80d879d59fc7d3968777b1f8c7e2189b2c1
luca <luca.antilici@gmail.com>
2017-03-03 12:13:04 +0100

    Modified custom sttrategy DTO and converter

--- src/main/java/it/uiip/digitalgarage/roboadvice/logic/converter/CustomStrategyConverter.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/logic/converter/CustomStrategyConverter.java
@@ -3,6 +3,8 @@ package it.uiip.digitalgarage.roboadvice.logic.converter;
 import it.uiip.digitalgarage.roboadvice.persistence.entity.CustomStrategyEntity;
 import it.uiip.digitalgarage.roboadvice.service.dto.CustomStrategyDTO;
 
+import java.util.List;
+
 /**
  * Created by Luca on 02/03/2017.
  */
@@ -11,7 +13,6 @@ public class CustomStrategyConverter implements GenericConverter<CustomStrategyE
    @Override
     public CustomStrategyDTO convertToDTO(CustomStrategyEntity customerStrategyEntity){
        CustomStrategyDTO customerStrategyDTO = new CustomStrategyDTO();
-       customerStrategyDTO.setId(customerStrategyEntity.getId());
        customerStrategyDTO.setIdUser(customerStrategyEntity.getUser().getId());
        customerStrategyDTO.setIdAssetClass(customerStrategyEntity.getAssetClass().getId());
        customerStrategyDTO.setPercentage(customerStrategyEntity.getPercentage());
@@ -19,9 +20,21 @@ public class CustomStrategyConverter implements GenericConverter<CustomStrategyE
        return customerStrategyDTO;
    }
 
-   @Override
+    @Override
+    public List<CustomStrategyEntity> convertToEntity(List<CustomStrategyDTO> dto) {
+        return null;
+    }
+
+    @Override
+    public List<CustomStrategyDTO> convertToDTO(List<CustomStrategyEntity> entity) {
+        return null;
+    }
+
+    @Override
     public CustomStrategyEntity convertToEntity(CustomStrategyDTO customStrategyDTO){
         CustomStrategyEntity customStrategyEntity = new CustomStrategyEntity();
         return customStrategyEntity;
    }
+
+
 }
