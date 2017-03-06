00eb36af8e3654cbb2968b60331f68fb298240e2
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-06 08:25:53 +0100

    refactoring

--- src/main/java/it/uiip/digitalgarage/roboadvice/service/dto/CustomStrategyDTO.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/service/dto/CustomStrategyDTO.java
@@ -1,15 +1,9 @@
 package it.uiip.digitalgarage.roboadvice.service.dto;
 
-/**
- * Created by Luca on 02/03/2017.
- */
-
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public @Data class  CustomStrategyDTO {
 
@@ -27,6 +21,7 @@ public @Data class  CustomStrategyDTO {
     @NotNull
     private boolean active;
 
+    @NotNull
     private String date;
 
 }
