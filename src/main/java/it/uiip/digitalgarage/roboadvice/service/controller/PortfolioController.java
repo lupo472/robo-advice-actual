287544ba7dd453f9f284a4b075e00107c14eac51
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-06 14:31:15 +0100

    refactoring

--- src/main/java/it/uiip/digitalgarage/roboadvice/service/controller/PortfolioController.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/service/controller/PortfolioController.java
@@ -1,7 +1,6 @@
 package it.uiip.digitalgarage.roboadvice.service.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
