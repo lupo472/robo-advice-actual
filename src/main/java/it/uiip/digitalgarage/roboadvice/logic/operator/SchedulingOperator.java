800a0accb46d4cd944d44b59c4cd84993f90bc1b
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-06 19:15:11 +0100

    implemented Scheduler Spring

---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/logic/operator/SchedulingOperator.java
@@ -0,0 +1,25 @@
+package it.uiip.digitalgarage.roboadvice.logic.operator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import it.uiip.digitalgarage.roboadvice.persistence.repository.AssetRepository;
+import it.uiip.digitalgarage.roboadvice.persistence.repository.FinancialDataRepository;
+
+@Service
+public class SchedulingOperator {
+	
+	@Autowired
+	private FinancialDataRepository FinancialDataRep;
+	
+	@Autowired
+	private AssetRepository assetRep;
+	
+	@Scheduled(cron = "0 * * * * *")
+	public void scheduleTask() {
+		QuandlOperator quandlOp = new QuandlOperator(FinancialDataRep, assetRep);
+		quandlOp.updateFinancialDataSet();
+	}
+
+}
