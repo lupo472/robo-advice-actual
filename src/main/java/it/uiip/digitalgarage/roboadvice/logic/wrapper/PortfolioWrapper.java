08291f16c8fe327752d7fb79fdbdaf0a03f46e68
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-08 12:21:29 +0100

    refactoring wrapper - I need to set the percentage

--- src/main/java/it/uiip/digitalgarage/roboadvice/logic/wrapper/PortfolioWrapper.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/logic/wrapper/PortfolioWrapper.java
@@ -1,15 +1,18 @@
 package it.uiip.digitalgarage.roboadvice.logic.wrapper;
 
 import it.uiip.digitalgarage.roboadvice.logic.converter.AssetClassConverter;
-import it.uiip.digitalgarage.roboadvice.logic.converter.AssetConverter;
 import it.uiip.digitalgarage.roboadvice.persistence.entity.PortfolioEntity;
 import it.uiip.digitalgarage.roboadvice.persistence.entity.UserEntity;
+import it.uiip.digitalgarage.roboadvice.service.dto.AssetClassStrategyDTO;
 import it.uiip.digitalgarage.roboadvice.service.dto.PortfolioDTO;
 import it.uiip.digitalgarage.roboadvice.service.dto.PortfolioElementDTO;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PortfolioWrapper implements GenericWrapper<PortfolioEntity, PortfolioDTO> {
 
@@ -54,10 +57,27 @@ public class PortfolioWrapper implements GenericWrapper<PortfolioEntity, Portfol
     	dto.setDate(entityList.get(0).getDate().toString());
     	dto.setIdUser(entityList.get(0).getUser().getId());
     	//CREARE LISTA
+    	Map<Long, List<PortfolioEntity>> map = new HashMap<>();
     	for (PortfolioEntity entity : entityList) {
-			
+    		if(map.get(entity.getAssetClass().getId()) == null) {
+    			map.put(entity.getAssetClass().getId(), new ArrayList<>());
+    		}
+			map.get(entity.getAssetClass().getId()).add(entity);
 		}
-    	
+    	for (Long idAssetClass : map.keySet()) {
+			BigDecimal sum = new BigDecimal(0);
+			PortfolioElementDTO element = new PortfolioElementDTO();
+			for (PortfolioEntity entity : map.get(idAssetClass)) {
+				sum = sum.add(entity.getValue());
+			}
+			element.setValue(sum);
+			AssetClassStrategyDTO assetClassStrategy = new AssetClassStrategyDTO();
+			assetClassStrategy.setAssetClass(this.assetClassConv.convertToDTO(map.get(idAssetClass).get(0).getAssetClass()));
+			element.setAssetClassStrategy(assetClassStrategy);
+			elements.add(element);
+		}
+    	dto.setList(elements);
+    	return dto;
 //        PortfolioDTO dto = new PortfolioDTO();
 //        dto.setList(new ArrayList<>());
 //        dto.setIdUser(entityList.get(0).getUser().getId());
@@ -70,7 +90,6 @@ public class PortfolioWrapper implements GenericWrapper<PortfolioEntity, Portfol
 //            dto.getList().add(element);
 //        }
 //        return dto;
-    	return null;
     }
 
 }
