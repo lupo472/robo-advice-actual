b3e872a298301bc8416978571b3978cd6c838ecb
luca <luca.antilici@gmail.com>
2017-03-08 15:29:49 +0100

    Added method getCapitalPeriod

--- src/main/java/it/uiip/digitalgarage/roboadvice/logic/operator/CapitalOperator.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/logic/operator/CapitalOperator.java
@@ -2,6 +2,9 @@ package it.uiip.digitalgarage.roboadvice.logic.operator;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.uiip.digitalgarage.roboadvice.persistence.entity.CapitalEntity;
 import it.uiip.digitalgarage.roboadvice.persistence.entity.UserEntity;
@@ -10,6 +13,7 @@ import it.uiip.digitalgarage.roboadvice.persistence.repository.FinancialDataRepo
 import it.uiip.digitalgarage.roboadvice.persistence.repository.PortfolioRepository;
 import it.uiip.digitalgarage.roboadvice.service.dto.CapitalDTO;
 import it.uiip.digitalgarage.roboadvice.service.dto.CapitalResponseDTO;
+import it.uiip.digitalgarage.roboadvice.service.dto.DataRequestDTO;
 import it.uiip.digitalgarage.roboadvice.service.dto.UserLoggedDTO;
 
 public class CapitalOperator extends AbstractOperator {
@@ -69,5 +73,26 @@ public class CapitalOperator extends AbstractOperator {
 		}
 		return true;
 	}
-	
+
+	public List<CapitalResponseDTO> getCapitalPeriod(DataRequestDTO request) {
+		List<CapitalResponseDTO> response = new ArrayList<CapitalResponseDTO>();
+		List<CapitalEntity> entityList;
+		if(request.getPeriod() == 0) {
+			entityList = this.capitalRep.findByUserId(request.getId());
+
+		}
+		else {
+			LocalDate initialDate = LocalDate.now();
+			LocalDate finalDate = initialDate.minus(Period.ofDays(request.getPeriod()));
+			entityList = this.capitalRep.findByUserIdAndDateBetween(request.getId(), finalDate, initialDate);
+		}
+		if (entityList.isEmpty()) {
+			return null;
+		}
+		for(CapitalEntity entity : entityList){
+			CapitalResponseDTO dto = (CapitalResponseDTO) this.capitalConv.convertToDTO(entity);
+			response.add(dto);
+		}
+		return  response;
+	}
 }
