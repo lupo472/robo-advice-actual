e0e4648099a0499ae8470e92b68472c906abcb65
luca <luca.antilici@gmail.com>
2017-03-09 18:39:32 +0100

    Changed the AssetClassRepository so that now uses standard spring data query for findAll ordered by id

--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/repository/AssetClassRepository.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/repository/AssetClassRepository.java
@@ -13,8 +13,8 @@ import it.uiip.digitalgarage.roboadvice.persistence.entity.AssetClassEntity;
 @Transactional
 public interface AssetClassRepository extends PagingAndSortingRepository<AssetClassEntity, Long> {
 
-	@Query(value = "SELECT * FROM asset_class ORDER BY id", nativeQuery = true)
-	public List<AssetClassEntity> findAll();
+	//@Query(value = "SELECT * FROM asset_class ORDER BY id", nativeQuery = true)
+	public List<AssetClassEntity> findAllOrderById();
 
 	public AssetClassEntity findById(Long id);
 		
