6c3d3fbb5e286e466e237e6dfb3ef18a3261034a
harrier91 <andreamaga4@gmail.com>
2017-03-10 19:55:09 +0100

    piechart in strategy

--- src/main/webapp/app/dashboard/strategy-graph/strategy-graph.component.ts
+++ src/main/webapp/app/dashboard/strategy-graph/strategy-graph.component.ts
@@ -1,3 +1,5 @@
+import { AppService } from '../../services/app.service';
+import { UserService } from '../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,40 +9,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StrategyGraphComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AppService: AppService, private UserService: UserService) { }
 
-  public brandPrimary:string =  '#20a8d8';
-  public brandSuccess:string =  '#4dbd74';
-  public brandInfo:string =   '#63c2de';
-  public brandWarning:string =  '#f8cb00';
-  public brandDanger:string =   '#f86c6b';
+  public labels: Array<string> = [];
+  public data: Array<number> = [];
+  public date: string;
+
+  public render: boolean = false;
+
+  ngOnInit() {
+
+    var user = this.UserService.getUser();
+    this.AppService.getActiveStrategy(user).subscribe(res => this.getStrategy(res.data));
+  }
+
+  getStrategy(data) {
+    console.log("strategy: ", data);
+
+    this.date = data.date;
+
+    data.list.forEach((item, index) => {
+      this.labels[index] = item.assetClass.name;
+      this.data[index] = item.percentage;
+    })
+
+    this.render = true;
+  }
+
+  public brandPrimary: string = '#20a8d8';
+  public brandSuccess: string = '#4dbd74';
+  public brandInfo: string = '#63c2de';
+  public brandWarning: string = '#f8cb00';
+  public brandDanger: string = '#f86c6b';
 
   //convert Hex to RGBA
-  public convertHex(hex:string,opacity:number){
-    hex = hex.replace('#','');
-    let r = parseInt(hex.substring(0,2), 16);
-    let g = parseInt(hex.substring(2,4), 16);
-    let b = parseInt(hex.substring(4,6), 16);
+  public convertHex(hex: string, opacity: number) {
+    hex = hex.replace('#', '');
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
 
-    let rgba = 'rgba('+r+','+g+','+b+','+opacity/100+')';
+    let rgba = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')';
     return rgba;
   }
 
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
 
-  public chartHovered(e:any):void {
+  public chartHovered(e: any): void {
     console.log(e);
   }
 
 
   // Pie
-  public pieChartLabels:string[] = ['Bonds', 'Stocks', 'Forex', 'Commodities'];
-  public pieChartData:number[] = [65, 10, 15,10];
-  public pieChartType:string = 'pie';
-  ngOnInit() {
-  }
+  public colors = [
+    {
+      backgroundColor: [this.brandSuccess,
+      this.brandInfo,
+      this.brandWarning,
+      this.brandDanger], borderWidth: 3
+    }];
+  public strategyOptions: any = {
+    maintainAspectRatio: false,
+    cutoutPercentage: 20,
+    legend: {
+      display: false
+    }
+  };
+  public pieChartType: string = 'doughnut';
 
 }
