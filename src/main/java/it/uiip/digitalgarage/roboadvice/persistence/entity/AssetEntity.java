b85d3ff3acbcade824d65ab9d4d8d7cf77ee9ef0
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-15 19:24:55 +0100

    AssetClass in AssetEntity is fetched LAZY

--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/entity/AssetEntity.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/entity/AssetEntity.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,7 +23,7 @@ public @Data class AssetEntity {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "id_asset_class", nullable = false)
 	private AssetClassEntity assetClass;
 
