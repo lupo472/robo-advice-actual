37df9e1c97569693a7bef5a6a80d588fb9e9eeb7
Cyro <c.infante89@gmail.com>
2017-03-16 15:48:19 +0100

    graph history of my strategies

--- src/main/webapp/app/dashboard/hystory-strategy/hystory-strategy.component.ts
+++ src/main/webapp/app/dashboard/hystory-strategy/hystory-strategy.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import {BaseChartDirective} from "ng2-charts";
 
 @Component({
   selector: 'app-hystory-strategy',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HystoryStrategyComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
   strategies:any;
-  chartData=[];
-  numOfClasses=4;
+  public period = 150;
+  public startdate: Date;
+  public chartData=[];
+  public dataclass=["bonds","forex","stocks","commodities"]; // da sostituire con il model
+  public colorclass=["#4dbd74","#63c2de","#f8cb00","#f86c6b"]; // da sostituire con il model
+  public labels=[];
+  public dati=[];
+
 
   res={
     "response": 1,
@@ -35,6 +44,38 @@ export class HystoryStrategyComponent implements OnInit {
           {
             "id": 1,
             "name": "bonds",
+            "percentage": 90
+          },
+          {
+            "id": 4,
+            "name": "commodities",
+            "percentage": 10
+          }
+        ],
+        "active": true,
+        "date": "2017-02-10"
+      },
+      {
+        "list": [
+          {
+            "id": 1,
+            "name": "bonds",
+            "percentage": 50
+          },
+          {
+            "id": 4,
+            "name": "commodities",
+            "percentage": 50
+          }
+        ],
+        "active": true,
+        "date": "2017-02-01"
+      },
+      {
+        "list": [
+          {
+            "id": 1,
+            "name": "bonds",
             "percentage": 2
           },
           {
@@ -45,53 +86,93 @@ export class HystoryStrategyComponent implements OnInit {
           {
             "id": 3,
             "name": "stocks",
-            "percentage": 56
+            "percentage": 50
+          },
+          {
+            "id": 4,
+            "name": "commodities",
+            "percentage": 6
           }
         ],
         "active": false,
-        "date": "2017-03-10"
+        "date": "2016-11-10"
       }
     ]
   };
 
-  dataset=[];
-  labels=[];
-  dataclass=[];
-  dati=[];
-
-  constructor() { }
+  constructor() {
+    this.startdate=new Date();
+    this.startdate.setDate(this.startdate.getDate() - this.period);
+    console.log("this.startdate: "+this.startdate);
+  }
 
   ngOnInit() {
     this.strategies = this.res;
+    this.getStrategies();
+
+  }
+
+  refreshData(){
+    this.dati=[];
+    this.chartData=[];
+    this.labels=[];
+    this.getStrategies();
+    //this.chart.chart.update();
+  }
+
+  getStrategies(){
     if(this.strategies.response == 1 ){
       this.strategies.data.forEach((strategy,i)=>{
-        this.labels.push(strategy.date);
-        strategy.list.forEach((classe,j)=>{
-          var idClasse=classe.id;
-          if(this.dati[idClasse-1]==undefined){
-            this.dati[idClasse-1]=new Array(this.strategies.data.length);
-          }
-          this.dati[idClasse-1][i]=classe.percentage;
-        })
+        var beginning = new Date(strategy.date);
+        console.log("this.startdate :"+this.startdate);
+        if(beginning>=this.startdate){
+          console.log("strategia compresa nell'intervallo, date:"+beginning);
+          this.labels.push(strategy.date);
+          strategy.list.forEach((classe,j)=>{
+            var idClasse=classe.id;
+            if(this.dati[idClasse-1]==undefined){
+              this.dati[idClasse-1]=new Array(this.strategies.data.length);
+            }
+            this.dati[idClasse-1][i]=classe.percentage;
+          })
+        }
       })
+      for(var k=0;k<this.dataclass.length;k++){
+        if(this.dati[k]==undefined){
+          this.dati[k]=new Array(this.labels.length);
+        }
+      }
+
     }
+    console.log("PRIMA DEGLI ZERI ",this.dati);
     //INSERIMENTO ZERI
     for(var i=0;i<this.dati.length;i++){
       for(var j=0;j<this.dati[i].length;j++){
         if(this.dati[i][j]==undefined){
-          this.dati[i][j]={percentage:0,name:''};
+          this.dati[i][j]=0;
         }
       }
-    }
-    this.dataclass=["bonds","forex","stocks","commodities"];
+    }console.log("DOPO GLI ZERI ",this.dati);
 
     for(var i=0;i<this.dati.length;i++){
-      console.log("this.dati[i]: ",this.dati[i])
-      this.chartData.push({data: this.dati[i], label:this.dataclass[i]});
+      this.chartData.push({data: this.dati[i], label:this.dataclass[i], backgroundColor: this.colorclass[i]});
     }
     console.log("chartData: ",this.chartData);
+
+    this.refreshChart();
+
   }
 
+  refreshChart() {
+    setTimeout(() => {
+
+      if (this.chart.chart.config.data) {
+        this.chart.chart.config.data.labels = this.labels;
+        this.chart.chart.config.data.datasets = this.chartData;
+        this.chart.chart.update();
+      }
+    });
+  }
   // barChart
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
