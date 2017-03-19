266b3c8e57015d1eac70b3554a48c8656718b6c4
luca <luca.antilici@gmail.com>
2017-03-19 20:12:32 +0100

    Added test for loginUser and registerUser of userOperator class

---
+++ src/test/java/it/uiip/digitalgarage/roboadvice/test/service/UserOperatorTest.java
@@ -0,0 +1,70 @@
+package it.uiip.digitalgarage.roboadvice.test.service;
+
+import it.uiip.digitalgarage.roboadvice.logic.operator.UserOperator;
+import it.uiip.digitalgarage.roboadvice.persistence.entity.UserEntity;
+import it.uiip.digitalgarage.roboadvice.persistence.repository.UserRepository;
+import it.uiip.digitalgarage.roboadvice.service.dto.LoginDTO;
+import it.uiip.digitalgarage.roboadvice.service.dto.UserDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import it.uiip.digitalgarage.roboadvice.RoboadviceApplication;
+import it.uiip.digitalgarage.roboadvice.service.controller.UserController;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RoboadviceApplication.class)
+public class UserOperatorTest {
+
+	@InjectMocks
+	@Autowired
+	private UserOperator userOp;
+
+	@Mock
+	private UserRepository userRep;
+
+	private UserDTO userDTO;
+
+	@Before
+	public void setUpMock() {
+		MockitoAnnotations.initMocks(this);
+		userDTO = new UserDTO();
+		userDTO.setEmail("test@test.com");
+		userDTO.setPassword("123456");
+	}
+
+	@Test
+	public void loginUserTestUserNotExists() {
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(null);
+		userEntity.setEmail("test@test.com");
+		userEntity.setPassword("3n4kjb");
+		userEntity.setDate(LocalDate.now());
+		userEntity.setLastUpdate(LocalDate.now().minusDays(1));
+
+		when(userRep.findByEmail(userDTO.getEmail())).thenReturn(userEntity);
+		LoginDTO response = userOp.loginUser(userDTO);
+		assertNull(response);
+	}
+
+	@Test
+	public void registerUserTestUserAlreadyRegistered() {
+		when(userRep.findByEmail(userDTO.getEmail())).thenReturn(new UserEntity());
+		assertFalse(userOp.registerUser(userDTO));
+
+	}
+}
