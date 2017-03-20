5e0d949bd5092d4506b534cc306f1eadbccf5b55
Cyro <c.infante89@gmail.com>
2017-03-20 11:13:09 +0100

    test dei tests

--- src/main/webapp/app/model/asset-class.spec.ts
+++ src/main/webapp/app/model/asset-class.spec.ts
@@ -6,7 +6,18 @@ describe('AssetClass', () => {
   });
   it('should accept values in the constructor', ()=>{
     let assetClass = new AssetClass(1,'custom');
-    expect(assetClass.id).toEqual(1);
-    expect(assetClass.name).toEqual('custom');
+    let id = assetClass.getId();
+    let name= assetClass.getName();
+    expect(id).toEqual(1);
+    expect(name).toEqual('custom');
+  });
+  it('should accept values in the set methods', ()=>{
+    let assetClass = new AssetClass();
+    assetClass.setId(1);
+    assetClass.setName("custom");
+    let id = assetClass.getId();
+    let name= assetClass.getName();
+    expect(id).toEqual(1);
+    expect(name).toEqual('custom');
   });
 });
