806bcdd3933f7057e991e2fc4768693caecedecc
luca <luca.antilici@gmail.com>
2017-03-22 14:05:51 +0100

    Added test for customStrategy and capital controllers, plus refactor of operators tests

--- src/test/java/it/uiip/digitalgarage/roboadvice/test/TestSuite.java
+++ src/test/java/it/uiip/digitalgarage/roboadvice/test/TestSuite.java
@@ -1,5 +1,7 @@
 package it.uiip.digitalgarage.roboadvice.test;
 
+import it.uiip.digitalgarage.roboadvice.test.controller.CapitalControllerTest;
+import it.uiip.digitalgarage.roboadvice.test.controller.CustomStrategyControllerTest;
 import it.uiip.digitalgarage.roboadvice.test.service.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -8,7 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ AssetClassOperatorTest.class, CapitalOperatorTest.class,
 		CustomStrategyOperatorTest.class, DefaultStrategyOperatorTest.class, FinancialDataTest.class,
-		PortfolioOperatorTest.class, UserOperatorTest.class})
+		PortfolioOperatorTest.class, UserOperatorTest.class, /*CustomStrategyControllerTest.class, CapitalControllerTest.class*/})
 public class TestSuite {
 
 }
