41331a329dbed7f2f42204a39bcfb3bec5666ad6
alberto.giovanelli <alberto.giovanelli@icloud.com>
2017-03-27 22:51:50 +0200

    adding tab module

--- src/main/webapp/app/services/app.config.ts
+++ src/main/webapp/app/services/app.config.ts
@@ -4,12 +4,9 @@ export interface IAppConfig {
 
 export const AppConfig: IAppConfig = {
 
-<<<<<<< HEAD
-    //url: 'http://192.168.2.102:8080/roboadvice/'
-    url: 'http://localhost:8080/roboadvice/'
-=======
-    url: 'http://192.168.2.112:8080/roboadvice/'
+
+    url: 'http://192.168.1.116:8080/roboadvice/'
     //url: 'http://localhost:8080/roboadvice/'
->>>>>>> d593f171fae51d77ee146caab09909c46b34d5a0
+
 
 };
--- src/main/webapp/app/shared/shared-module/shared-module.module.ts
+++ src/main/webapp/app/shared/shared-module/shared-module.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DetailMarketValueChartComponent} from "../../edit/detail-market-value-chart/detail-market-value-chart.component";
 import { AmChartsModule } from "amcharts3-angular2";
-
+import {TabMenuModule,MenuItem} from 'primeng/primeng';
 @NgModule({
   imports: [
     CommonModule,
-    AmChartsModule
+    AmChartsModule,
+      TabMenuModule
   ],
   declarations: [
       DetailMarketValueChartComponent
