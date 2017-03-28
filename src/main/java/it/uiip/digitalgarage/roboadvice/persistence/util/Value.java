56bd7e1715475113ae6a9cce88d035a147d8061d
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-28 16:33:22 +0200

    completed documentation for util

--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/util/Value.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/util/Value.java
@@ -6,6 +6,11 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * This class represents an Object used in PortfolioRepository with LocalDate date and BigDecimal value.
+ *
+ * @author Cristian Laurini
+ */
 public @Data @AllArgsConstructor class Value {
 
 	private LocalDate date;
