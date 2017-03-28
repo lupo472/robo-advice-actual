27eadb843a3736c757364ff129efb0ff9216eb83
Cristian Laurini <cristian.laurini@gmail.com>
2017-03-28 12:27:25 +0200

    created documentation for Util

--- src/main/java/it/uiip/digitalgarage/roboadvice/service/util/HashFunction.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/service/util/HashFunction.java
@@ -2,10 +2,20 @@ package it.uiip.digitalgarage.roboadvice.service.util;
 
 import java.security.MessageDigest;
 
+/**
+ * This class offers a method to hash a String.
+ *
+ * @author Luca Antilici
+ */
 public class HashFunction {
 
+	/**
+	 * This method is useful to hash a String.
+	 *
+	 * @param input		String to hash.
+	 * @return			String that represents the hashed input String.
+	 */
     public static String hashStringSHA256(String input){
-
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
@@ -20,4 +30,5 @@ public class HashFunction {
             return e.getLocalizedMessage();
         }
     }
+
 }
